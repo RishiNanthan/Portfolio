e9f3a4a212d10851e3e7c67137f5cf95b5511c2d
Rishi Nanthan <rishinanthan344@gmail.com>
2020-09-27 19:53:34 +0530

    Updates

---
+++ precache-manifest.bb7bb529739ce50fc9619986db9946a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bada3c0ac1b92bb8004a212e52d96f09",
+    "url": "//rishinanthan.github.io/Portfolio/index.html"
+  },
+  {
+    "revision": "201a58420d5375055918",
+    "url": "//rishinanthan.github.io/Portfolio/static/css/main.b89fbc1d.chunk.css"
+  },
+  {
+    "revision": "b6a27264c6901f7460be",
+    "url": "//rishinanthan.github.io/Portfolio/static/js/2.abc48b42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//rishinanthan.github.io/Portfolio/static/js/2.abc48b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201a58420d5375055918",
+    "url": "//rishinanthan.github.io/Portfolio/static/js/main.d62ebf16.chunk.js"
+  },
+  {
+    "revision": "c4d6c028746fff20a42b",
+    "url": "//rishinanthan.github.io/Portfolio/static/js/runtime-main.f138045d.js"
+  },
+  {
+    "revision": "37352908545133f9701c378f522a6584",
+    "url": "//rishinanthan.github.io/Portfolio/static/media/profile.37352908.jpeg"
+  }
+]);
